c5f3ae8ae301887f1250118c60976001fc5db2d9
Ali Hamzehpour <alihamzehpur2002@gmail.com>
2023-03-28 20:04:02 +0430

    Fix mux

--- CA1/components/mux.v
+++ CA1/components/mux.v
@@ -1,14 +1,14 @@
 module mux_4to1 (i0, i1, i2, i3, sel, out);
     input i0, i1, i2, i3;
     input [1:0] sel;
-    output reg out;
+    output reg [3:0] out;
     always @(i0 or i1 or i2 or i3 or sel)begin
         case (sel)
             2'b00 : out = i0;
             2'b01 : out = i1;
             2'b10 : out = i2;
             2'b11 : out = i3;
-            default: out = 1'bx;
+            default: out = 4'bx;
         endcase
     end
 endmodule
